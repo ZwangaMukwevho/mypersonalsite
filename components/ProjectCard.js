7e558785ad9b3b007cba579c37393eaca4736b7e
ZwangaMukwevho <zwangatm3@gmail.com>
2024-06-19 20:22:15 +0200

    fix: linting

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -4,10 +4,10 @@ import { FaGithub, FaExternalLinkSquareAlt } from 'react-icons/fa'
 
 const ProjectCard = ({ title, description, img, githubUrl, projectUrl, tech, dataTestid }) => (
   <div
-    className="max-w-xs overflow-hidden rounded-xl shadow-lg dark:bg-gray-600 dark:shadow-cyan-600/50 h-100"
+    className="h-100 max-w-xs overflow-hidden rounded-xl shadow-lg dark:bg-gray-600 dark:shadow-cyan-600/50"
     data-testid={dataTestid}
   >
-     <img src={img} alt={title} className="object-cover w-full h-48" />
+    <img src={img} alt={title} className="h-48 w-full object-cover" />
     <div className="px-6 py-4">
       <div className="mb-2 text-xl font-bold">{title}</div>
       <p className="text-base text-gray-500 dark:text-gray-100">{description}</p>
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,16 +6,16 @@ const siteMetadata = {
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://zwangamukwevho.co.za/',
-  siteRepo: 'https://github.com/chrisotto6/chrisotto',
+  siteRepo: 'https://github.com/ZwangaMukwevho/mypersonalsite',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.jpg',
   socialBanner: '/static/images/twitter-card.png',
   email: 'zwangatm3@gmail.com',
-  github: 'https://github.com/ZwangaMukwevho',
-  twitter: 'https://twitter.com/chris_otto6',
+  github: 'https://github.com/ZwangaMukwevho/',
+  twitter: 'https://twitter.com/zwangamukwevho',
   // facebook: 'https://facebook.com',
   // youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com/in/zwanga-mukwevho-386320159/',
+  linkedin: 'https://www.linkedin.com/in/zwanga-mukwevho-386320159/mypersonalsite',
   locale: 'en-US',
   analytics: {
     googleAnalyticsId: 'G-9LHN1C72K7', // e.g. UA-000000-2 or G-XXXXXXX
